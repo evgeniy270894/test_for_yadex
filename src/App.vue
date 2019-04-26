ab950b5de4bc86f2929f2b08aa0c7cd395fc8d7e
Галицын Евгений Сергеевич <ysgalits@mts.ru>
2019-04-26 19:56:26 +0300

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app id="inspire">
+        <v-toolbar color="indigo" dark fixed app>
+            <v-toolbar-title>Табло аэропорта</v-toolbar-title>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout
+                        justify-center
+                        align-center
+                >
+                </v-layout>
+            </v-container>
+        </v-content>
+        <v-footer color="indigo" app>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data: () => ({
+            drawer: null
+        }),
+        props: {
+            source: String
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+});
+
 
 new Vue({
   render: h => h(App),
